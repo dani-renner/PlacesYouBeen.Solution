4d277c06ea63bec63580bb82867d180ebc440bfc
Dani Renner <danijrenner@gmail.com>
2021-03-03 15:58:27 -0800

    start travel diary project
    Co-authored-by:Woo Jin Kim <kimwoojin211@gmail.com>
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>

---
+++ PlacesYouBeen/Models/Place.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+
+namespace PlacesYouBeen.Models{
+  public class Place
+  {
+    public string Location { get; set; }
+    public string Stay { get; set; }
+    public string Buddies { get; set; }
+    public string Notes { get; set; }
+
+    public int Id { get; }
+    private static List<Place> _instances = new List<Place>{};
+
+    public Place(string location, string stay, string buddies, string notes)
+    {
+      Location = location;
+      Stay = stay;
+      Buddies = buddies;
+      Notes = notes;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+public static List<Place> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Place Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Places You've Been
+### A project for Epicodus
+
+#### Users can track where they've traveled to
+
+#### By Dani Renner, Woo Jin Kim, and Scott O'Neil
+## Technologies Used
+
+* C#
+* ASP.NET
+
+## Description
+
+
+## Setup/Installation Requirements
+
+* You can clone the repository to your desktop
+* Navigate to the JobBoard directory
+* Enter "dotnet run" (without the quotes) in the terminal to start
+* Use Ctrl + C to escape or quit the program if necessary
+
+## License
+
+[MIT](https://opensource.org/licenses/MIT)
+
+Copyright © 2021 Dani Renner, Woo Jin Kim, and Scott O'Neil
+
+All Rights Reserved

ca6229c228aad0a02ee8800448a957bfeeb68ba1
Dani Renner <danijrenner@gmail.com>
2021-03-03 16:17:48 -0800

    add cshtml to add places
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>
    Co-authored-by:Woo Jin Kim <kimwoojin211@gmail.com>

--- PlacesYouBeen/Controllers/PlacesController.cs
+++ PlacesYouBeen/Controllers/PlacesController.cs
@@ -1,2 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using PlacesYouBeen.Models;
+using System.Collections.Generic;
+
+namespace PlacesYouBeen.Controllers
+{
+  public class PlacesController : Controller
+  {
+  [HttpGet("/places")]
+    public ActionResult Index()
+    {
+      List<Place> allPlaces = Place.GetAll();
+      return View(allPlaces);
+    }
+    [HttpGet("/places/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+  }
+}
